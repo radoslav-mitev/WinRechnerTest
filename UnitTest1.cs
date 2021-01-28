80be1d0488acd7288263734c2bf2576e6f885207
user320 <radoslav.mitev@gmx.at>
2021-01-28 14:54:04 +0100

    xpath-locator-test and parameterized tests added

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -12,7 +12,6 @@ namespace WinRechnerTest
         private const string CalculatorAppId = "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App";
         private WindowsDriver<WindowsElement> driver;
 
-
         [OneTimeSetUp]
         public void Setup()
         {
@@ -47,6 +46,31 @@ namespace WinRechnerTest
             this.driver.FindElementByAccessibilityId("num1Button").Click();
             this.driver.FindElementByAccessibilityId("equalButton").Click();
             Assert.AreEqual("8.", GetCalculatorResultText());
+        }
+
+        [Test]
+        public void Subtraction()
+        {
+            // Find the buttons by their accessibility ids using XPath and click them in sequence to perform 9 - 1 = 8
+            this.driver.FindElementByXPath(".//*[@AutomationId=\"num9Button\"]").Click();
+            this.driver.FindElementByXPath("//Button[@AutomationId=\"minusButton\"]").Click();
+            this.driver.FindElementByXPath("//Button[@AutomationId=\"num1Button\"]").Click();
+            this.driver.FindElementByXPath("//Button[@AutomationId=\"equalButton\"]").Click();
+            Assert.AreEqual("8.", GetCalculatorResultText());
+        }
+
+        [Test]
+        [TestCase("Eins", "Plus", "Sieben", "8.")]
+        [TestCase("Neun", "Minus", "Eins", "8.")]
+        [TestCase("Acht", "Dividieren durch", "Acht", "1.")]
+        public void SolvingAnEquation(string input1, string operation, string input2, string expectedResult)
+        {
+            // Run sequence of button presses specified above and validate the results
+            this.driver.FindElementByName(input1).Click();
+            this.driver.FindElementByName(operation).Click();
+            this.driver.FindElementByName(input2).Click();
+            this.driver.FindElementByName("Gleich").Click();
+            Assert.AreEqual(expectedResult, GetCalculatorResultText());
         }
 
         private string GetCalculatorResultText()
